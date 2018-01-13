77d02b05c058e21ab69d89bfef05e4a0e9b95738
Amit Verma <amit.verma2@gmail.com>
2018-01-14 00:50:25 +0530

    added carlos dfs implementation. Carlos is not yet solved.

--- src/carlos-inversion/carlos.c
+++ src/carlos-inversion/carlos.c
@@ -36,4 +36,115 @@ Output
 For each test case, output a single line containing -1 if it's impossible to
 make the letters of W sorted in alphabetical order. Otherwise, output the
 smallest number of letters that need to be transformed to achieve that.
+
+Constraints
+1 ≤ T ≤ 10
+1 ≤ N ≤ 200000
+0 ≤ K ≤ M(M-1)/2
+1 ≤ x,y ≤ M
+all unordered pairs (x,y) will be distinct
+1 ≤ ai ≤ M
+
+Subtask 1 (18 points):
+
+1 ≤ M ≤ 10
+1 ≤ N ≤ 20
+Subtask 2 (60 points):
+
+1 ≤ M ≤ 50
+1 ≤ N ≤ 20000
+Subtask 3 (22 points):
+
+1 ≤ M ≤ 200
+1 ≤ N ≤ 200000
+
+Example
+Input:
+2
+3 3 3
+1 2
+2 3
+3 1
+3 2 1
+3 1 2
+2 3
+2 1
+Output:
+2
+-1
+
 */
+
+#include <stdio.h>
+#include <string.h>
+
+int graph[200+1][200+1];
+int visited[200+1];
+int smallest[200+1];
+int A[200001];
+int M, K, N;
+
+void reset_dp(void)
+{
+    memset(graph, 0, sizeof(graph));
+    memset(visited, 0, sizeof(visited));
+    memset(A, 0, sizeof(A));
+    memset(smallest, 0, sizeof(smallest));
+}
+
+void dfs(int i, int *min)
+{
+    if(visited[i])
+        return;
+    visited[i] = 1;
+    printf("... visited %d\n", i);
+    if(i < *min)
+        *min = i;
+    for(int v = 1; v <= M; v++) {
+        if(graph[i][v] && !visited[v]) {
+            printf("From %d\n", i);
+            dfs(v, min);
+        }
+    }
+}
+
+int find_smallest_alphabet(int i)
+{
+    if(smallest[i])
+        return smallest[i];
+    memset(visited, 0, sizeof(visited));
+    int min = i;
+    printf("*** starting dfs for %d\n", i);
+    dfs(i, &min);
+    smallest[i] = min;
+    return smallest[i];
+}
+
+int main(void)
+{
+    int T;
+    scanf("%d", &T);
+    while(T--) {
+        reset_dp();
+        int changes = 0;
+        scanf("%d %d %d", &M, &K, &N);
+        for(int k = 1 ; k <= K; k++) {
+            int x, y;
+            scanf("%d %d", &x, &y);
+            graph[x][y] = 1;
+            graph[y][x] = 1;
+        }
+        for(int i = 1 ; i <= N; i++)
+           scanf("%d", &A[i]);
+
+        for(int m = 1; m <= M; m++)
+            find_smallest_alphabet(m);
+
+        printf("Smallest alphabet:\n");
+        for(int m = 1; m <= M; m++)
+            printf("%d => %d\n", m, smallest[m]);
+
+        printf("%d\n", changes);
+    }
+    return 0;
+}
