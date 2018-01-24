d08e6436c191d846878ba70c50328e2696bd3a02
Amit Verma <amit.verma2@gmail.com>
2018-01-25 02:43:43 +0530

    added longlongnim problem statement

---
+++ src/longlongnim/longlongnim.cpp
@@ -0,0 +1,16 @@
+/*
+ * Long Long Nim
+ *
+ * https://community.topcoder.com/stat?c=problem_statement&pm=6856&rd=10010
+ *
+ * LongLongNim is a game played by two players who alternate turns. n coins
+ * are arranged in a pack. A valid move consists of removing exactly X coins
+ * from the pack, where X is an element of the set moves. If a player cannot
+ * perform a valid move during his turn, he loses the game, and the other
+ * player, of course, wins.
+ *
+ * You are given the set moves as a vector <int>, and you are also given an
+ * int maxN. Return the number of different values of n between 1 and maxN,
+ * inclusive, that make the game possible to win for the second player
+ * (assuming both players play optimally).
+ */
