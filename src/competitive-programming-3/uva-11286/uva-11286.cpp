cf71ce97810bd6bdd817a2c441e64d90b4a39f06
Amit Verma <amit.verma2@gmail.com>
2018-04-02 19:03:34 +0530

    updated problem statement for uva 11286

--- src/competitive-programming-3/uva-11286/uva-11286.cpp
+++ src/competitive-programming-3/uva-11286/uva-11286.cpp
@@ -1,3 +1,44 @@
+/*
+ * Frosh commencing their studies
+ * at Waterloo have diverse
+ * interests, as evidenced by
+ * their desire to take various
+ * combinations of courses
+ * from among those available.
+ * University administrators
+ * are uncomfortable with this
+ * situation, and therefore wish
+ * to offer a conformity prize
+ * to frosh who choose the
+ * most popular combination of
+ * courses. How many frosh
+ * will win the prize?
+ * Input
+ * The input consists of several test cases followed by a line containing ‘0’.
+ * Each test case begins with an integer 1 ≤ n ≤ 10000, the number of frosh.
+ * For each frosh, a line follows containing the course numbers of five
+ * distinct courses selected by the frosh. Each course number is an integer
+ * between 100 and 499.
+ * The popularity of a combination is the number of frosh selecting exactly the
+ * same combination of courses. A combination of courses is considered most
+ * popular if no other combination has higher popularity.
+ * Output
+ * For each line of input, you should output a single line giving the total
+ * number of students taking some combination of courses that is most popular.
+ * Sample Input
+ * 3
+ * 100 101 102 103 488
+ * 100 200 300 101 102
+ * 103 102 101 488 100
+ * 3
+ * 200 202 204 206 208
+ * 123 234 345 456 321
+ * 100 200 300 400 444
+ * 0
+ * Sample Output
+ * 2
+ * 3
+ */
 #include<iostream>
 #include<algorithm>
 #include<map>
